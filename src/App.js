eaa5a8e14b8570807ae8bbb800ffb8318aa14d21
Adil <ajkkalappatt@gmail.com>
2022-06-20 10:05:21 +0530

    uploading2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Signin from './Components/Signin/Signin';
-import {BrowserRouter as Router, Route,Switch} from  'react-router-dom';
+import {BrowserRouter as Router, Route} from  'react-router-dom';
 import Login from './Components/Login/Login';
 import Home from './Components/Home/Home';
 import Main from './Components/Main/Main';
@@ -9,26 +9,23 @@ function App() {
   return (
     <Router>
     <div className="App">
-      <Switch>
+      
         <Route exact path="/"  >
           <Home/>
         </Route>
-       </Switch>
-       <Switch>
+       
         <Route exact path="/main"  >
           <Main/>
         </Route>
-       </Switch>
-        <Switch>
+      
         <Route exact path="/login"  >
           <Login/>
         </Route>
-        </Switch>
-      <Switch>
+       
         <Route exact path="/signin">
           <Signin/>
         </Route>
-      </Switch>
+      
     </div>
     </Router>
   );
